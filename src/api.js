9080baee9def47157f8da795c6e11b79d8ca0c96
tamgamer97 <tamimmiah7002@gmail.com>
2021-10-01 23:04:35 +0100

    again

--- src/api.js
+++ src/api.js
@@ -9,11 +9,8 @@ const fetch = require('cross-fetch');
 // routes
 
 router.get("/", async (req, res) => {
-    
-  let data = await recData()
 
-  console.log("data")
-  console.log(data)
+  data = await recData()
 
   res.json({
     data: data
@@ -37,6 +34,12 @@ async function recData()
 app.use(`/.netlify/functions/api`, router);
 
 module.exports = app;
+module.exports = {
+  app,
+  externals: {
+    'cross-fetch': 'commonjs2 cross-fetch',
+  }
+};
 module.exports.handler = serverless(app);
 // npm install express netlify-lambda serverless-http 
 // http://localhost:9000/.netlify/functions/api
